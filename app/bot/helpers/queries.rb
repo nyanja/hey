71c3b2ca6a18d126b8dea9af3b9d6f355e8a0ce6
nyan <1eaegue@gmail.com>
2018-10-01 20:08:43 +0300

    bugfixes

--- app/bot/helpers/queries.rb
+++ app/bot/helpers/queries.rb
@@ -2,24 +2,25 @@ module Bot
   module Helpers
     module Queries
       def count_this_query
-        Storage.incr("qc//#{query}")
+        Bot::Storage.incr("qc//#{query}")
+        puts Bot::Storage.get "qc//#{query}"
       end
 
       def query_limited?
         c = config.query_skip_on_limit
-        return if c.nil? || c.zero? || Storage.get("qc//#{query}").to_i <= c
-        Storage.set("delay//#{query}",
-                    Time.now.to_i + config.query_skip_on_limit_interval)
-        Strorage.del("qc//#{query}")
+        return if c.nil? || c.zero? || Bot::Storage.get("qc//#{query}").to_i < c
+        Bot::Storage.set("delay//#{query}",
+                         Time.now.to_i + config.query_skip_on_limit_interval)
+        Bot::Storage.del("qc//#{query}")
         true
       end
 
       def query_delayed?
-        t = Storage.get("delay//#{query}") ||
-            Storage.get("delay//#{query} #{driver.device}")
+        t = Bot::Storage.get("delay//#{query}") ||
+            Bot::Storage.get("delay//#{query} #{driver.device}")
         return true if t && t.to_i < Time.now.to_i
-        Storage.del("delay//#{query}")
-        Storage.del("delay//#{query} #{driver.device}")
+        Bot::Storage.del("delay//#{query}")
+        Bot::Storage.del("delay//#{query} #{driver.device}")
         false
       end
     end
