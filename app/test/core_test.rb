927d935d3afda0a48da27a5d6622f686535dc9d9
Aeonax Liar <aeonax.liar@gmail.com>
2019-02-01 14:33:53 +0200

    Runner removed

--- app/test/core_test.rb
+++ app/test/core_test.rb
@@ -2,20 +2,7 @@
 
 require "minitest/autorun"
 
-require_relative "../bot/helpers/logger"
-require_relative "../bot/helpers/wait"
-require_relative "../bot/helpers/exception_handler"
-require_relative "../bot/helpers/sites"
-require_relative "../bot/storage"
-require_relative "../bot/ip"
-
-require_relative "../bot/helpers/config"
-require_relative "../bot/helpers/queries"
-require_relative "../bot/helpers/results"
-require_relative "../bot/scenarios/single"
-require_relative "../bot/runner"
-require_relative "../bot/driver"
-require_relative "../bot/core"
+require_relative "../bot"
 
 class CoreTest < Minitest::Test
   def setup
